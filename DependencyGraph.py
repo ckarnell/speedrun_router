e628dd09c67df42ee42cf6c6f89a0c81218940b0
Cohen Karnell <cohen.karnell@gmail.com>
2017-03-11 00:44:55 -0500

    Add incomplete method for Dijkstra's algorithm

--- DependencyGraph.py
+++ DependencyGraph.py
@@ -122,22 +122,41 @@ class DependencyGraph():
         dependents = list(set(dependents)) # Get rid of duplicates
         for dep in dependents:
             if dep not in visited:
-                dependents.extend(self.getListOfDependentNodesRecurse(dep, dependents, visited))
+                dependents += (self.getListOfDependentNodesRecurse(dep, dependents, visited))
         dependents = list(set(dependents))
         return dependents
 
-    def minimizeGraph(self, origin_node=None, terminal_node=None):
-        if origin_node:
-            self.setOriginNode(origin_node)
-        if terminal_node:
-            self.setTerminalNode(terminal_node)
-        if self.origin_node is None or self.terminal_node is None:
-            raise Exception('Both origin and terminal nodes must be set')
-
+    def minimizeGraph(self):
         nodes_without_origin = self.dep_dict.keys()
         nodes_without_origin.remove(self.origin_node)
         if any(self.dep_dict[node] == {} for node in nodes_without_origin):
             raise Exception('All nodes except the origin node must have at least one dependence')
+
+    def applyDijkstra(self):
+        '''
+        Performs a number of checks before minimizing the dependency graph
+        and applying Dijkstra's algorithm to return the minimum time distance
+        route
+        '''
+        if self.origin_node is None or self.terminal_node is None:
+            raise Exception('Both origin and terminal nodes must be set')
+        nodes_without_origin = list(self.dep_dict)
+        nodes_without_origin.remove(self.origin_node)
+        if any(self.dep_dict[node] == {} for node in nodes_without_origin):
+            raise Exception('Every node must have at least one dependency except the origin')
+        for node in nodes_without_origin:
+            if any(self.dep_dict[node][dependency] is None for dependency in self.dep_dict[node]):
+                raise Exception('Every edge must be filled in with a time distance')
+
+        # Minimize graph and apply Dijkstra's algorithm
+        self.minimizeGraph()
+        unvisited_nodes = self.dep_dict.keys()
+        node_distances = {node: sys.maxint for node in unvisited_nodes}
+        node_distances[self.origin_node] = 0
+
+        while unvisited_nodes:
+            # TODO: Finish Dijkstra's algorithm here
+            pass
 
 if __name__ == '__main__':
     debug = True # Set to True to run tests
@@ -195,7 +214,9 @@ if __name__ == '__main__':
                 self.dep_graph.dep_dict = {'GameStart': {}, 'PokeyEscape': {'GetSword': 4},
                                            'GetSword': {'GameStart': 5}, 'WESS': {'PokeyEscape': 10}}
                 inputs = (('PokeyEscape', ['GameStart', 'GetSword']),
-                          ('WESS', ['GameStart', 'PokeyEscape', 'GetSword']))
+                          ('WESS', ['GameStart', 'PokeyEscape', 'GetSword']),
+                          ('GetSword', ['GameStart']),
+                          ('GameStart', []))
                 test_function = self.dep_graph.getListOfDependentNodesRecurse
                 self.assertTrue(all(test_function(i[0]) == i[1] for i in inputs))
 
